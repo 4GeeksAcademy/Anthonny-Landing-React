a3a4e2ccfe676983cec5bfc47459b09dc940b8c8
PugMau <166648867+PugMau@users.noreply.github.com>
2024-07-04 03:15:41 +0000

    Primer Cambio

---
+++ src/js/component/navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+
+const Navbar = () => {
+    return (<nav className="navbar navbar-light bg-light">
+        <a className="navbar-brand" href="#">Navbar</a>
+    </nav>)
+}
+
+export default Navbar
